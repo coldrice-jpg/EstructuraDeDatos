ae1332cde57f51d20285ecbf51870f9f267d9f4b
Alexis <al9817071@gmail.com>
2025-10-17 08:31:41 -0600

    Examen primer parcial Alexis

---
+++ src/com.mx.curso/unidad1/matriz/Examen_primer_parcial.py
@@ -0,0 +1,29 @@
+# Examen primer parcial
+
+# Creación de la función
+def diferencia(arr):
+    
+    # Declaración variables suma
+    suma_diagonal1 = 0
+    suma_diagonal2 = 0
+    
+    # Ciclo de suma
+    for i in range(len(arr)):
+        
+        suma_diagonal1 += arr[i][i]
+        
+        suma_diagonal2 += arr[i][(len(arr) - 1) - i]
+        
+    return abs(suma_diagonal1 - suma_diagonal2)
+    
+# Creación de la matriz
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+resultado = diferencia(matriz)
+
+print(resultado)
+
